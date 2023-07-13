d286e7a79b7594a5ca1969e5cbaec8432cd98821
noambassat <noambassat92@gmail.com>
2023-07-13 11:09:50 +0300

    new

--- index.js
+++ index.js
@@ -1,33 +1,70 @@
 const puppeteer = require('puppeteer');
+const axios = require('axios');
+
+const API_KEY = '3e751cb2-8ed2-4dfd-ac3a-d3f42605ca17';
 
 async function scrapeOncoKB() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.oncokb.org/actionableGenes'); // Replace with your target URL
+    await page.goto('https://www.oncokb.org/actionableGenes');
 
     const genes = {};
     const alterations = {};
 
+    const data = [];
+
     const rowElements = await page.$$('.rt-tr.-odd');
 
     for (const rowElement of rowElements) {
-        const links = await rowElement.$$('a');
+        const [genLinks, alterationLinks] = await rowElement.$$('a');
 
-        const geneHref = await (await links[0].getProperty('href')).jsonValue();
-        const geneText = await page.evaluate((link) => link.textContent, links[0]);
+        const geneHref = await (await genLinks.getProperty('href')).jsonValue();
+        const geneText = await page.evaluate((link) => link.textContent, genLinks);
 
-        const alterationsHref = await (await links[1].getProperty('href')).jsonValue();
-        const alterationsText = await page.evaluate((link) => link.textContent, links[1]);
+        const alterationsHref = await (await alterationLinks.getProperty('href')).jsonValue();
+        const alterationsText = await page.evaluate((link) => link.textContent, alterationLinks);
 
-        genes[geneText] = geneHref;
-        alterations[alterationsText] = alterationsHref
-
-
+        data.push({
+            geneHref,
+            geneText,
+            alterationsHref,
+            alterationsText
+        });
     }
 
-    console.log('Gene Links:', genes);
-    console.log('Gene geneTexts:', alterations);
+    for (const item of data) {
+        console.log(`Gene Href: ${item.geneHref}`);
+        console.log(`Gene Text: ${item.geneText}`);
+        console.log(`Alterations Href: ${item.alterationsHref}`);
+        console.log(`Alterations Text: ${item.alterationsText}`);
+        console.log('----------------------');
 
+        // Add code to make API requests and combine JSON object
+        const geneResponse = await axios.get(`https://www.oncokb.org/api/private/utils/numbers/gene/${item.geneHref}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${API_KEY}`
+            }
+        });
+
+        const variantResponse = await axios.get(`https://www.oncokb.org/api/v1/variants/lookup?hugoSymbol=${item.geneHref}&variant=${item.alterationsHref}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${API_KEY}`
+            }
+        });
+
+        const geneData = geneResponse.data;
+        const variantData = variantResponse.data;
+
+        // Combine gene and variant data into a single JSON object
+        const combinedItem = {
+            gene: geneData,
+            variant: variantData
+        };
+
+        console.log('Combined Data:', combinedItem);
+    }
 
     await browser.close();
 }
--- sivan.js
+++ sivan.js
@@ -1,60 +1,48 @@
+
+
+
 const puppeteer = require('puppeteer');
-const axios = require('axios');
 
 async function scrapeOncoKB() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.oncokb.org/actionableGenes');
+    await page.goto('https://www.oncokb.org/actionableGenes'); // Replace with your target URL
 
     const genes = {};
     const alterations = {};
 
+    const data = [];
+
     const rowElements = await page.$$('.rt-tr.-odd');
 
     for (const rowElement of rowElements) {
-        const genesLinks = await rowElement.$$('a');
+        const [genLinks, alterationLinks] = await rowElement.$$('a');
 
-        const geneHref = await (await genesLinks[0].getProperty('href')).jsonValue();
-        const geneText = await page.evaluate((link) => link.textContent, genesLinks[0]);
+        const geneHref = await (await genLinks.getProperty('href')).jsonValue();
+        const geneText = await page.evaluate((link) => link.textContent, genLinks);
 
-        const alterationsHref = await (await genesLinks[1].getProperty('href')).jsonValue();
-        const alterationsText = await page.evaluate((link) => link.textContent, genesLinks[1]);
+        const alterationsHref = await (await alterationLinks.getProperty('href')).jsonValue();
+        const alterationsText = await page.evaluate((link) => link.textContent, alterationLinks);
 
-        if (alterationsText.startsWith('Exon')) {
-            continue; // Skip alterations starting with 'Exon'
-        }
-
-        genes[geneText] = geneHref;
-        alterations[alterationsText] = alterationsHref;
+        data.push({
+            geneHref,
+            geneText,
+            alterationsHref,
+            alterationsText
+        });
     }
 
-    const combinedData = [];
 
-    for (const alterationText in alterations) {
-        const geneHref = genes[alterationText];
-        const alterationHref = alterations[alterationText];
-
-        try {
-            const geneResponse = await axios.get(`https://www.oncokb.org/api/private/utils/numbers${geneHref}`);
-            const variantResponse = await axios.get(`https://www.oncokb.org/api/v1/variants/lookup${alterationHref}`);
-
-            const geneData = geneResponse.data;
-            const variantData = variantResponse.data;
-
-            const combinedItem = {
-                gene: geneData,
-                variant: variantData,
-            };
-
-            combinedData.push(combinedItem);
-        } catch (error) {
-            console.error('Error fetching gene or variant data:', error);
-        }
+    for (const item of data) {
+        console.log(`Gene Href: ${item.geneHref}`);
+        console.log(`Gene Text: ${item.geneText}`);
+        console.log(`Alterations Href: ${item.alterationsHref}`);
+        console.log(`Alterations Text: ${item.alterationsText}`);
+        console.log('----------------------');
     }
-
-    console.log('Combined Data:', combinedData);
 
     await browser.close();
 }
 
 scrapeOncoKB();
+
